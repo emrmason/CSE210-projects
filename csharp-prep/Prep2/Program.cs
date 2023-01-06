f246058a4281deba1283df4f2cd59fcd62485230
emrmason <mas20021@byui.edu>
2023-01-06 13:56:19 -0800

    Prep1 and Prep 2, Week 1- Complete

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string gradeA = "You have an A. "; 
+        string gradeB = "You have a B. ";
+        string gradeC = "You have a C. ";
+        string gradeD = "You have a D. ";
+        string gradeF = "You have an F. ";
+        string gradePass = "You have passed the course. Congratulations!";
+        string gradeFail = "You have failed the course. Don't give up! Try again!";
+        Console.WriteLine("What is your grade percentage? ");
+        int percent = Convert.ToInt32(Console.ReadLine());
+
+        if (percent >= 90)
+        {
+            Console.WriteLine($"{gradeA}{gradePass}");
+        }
+        else if (percent >= 80)
+        {
+            Console.WriteLine($"{gradeB}{gradePass}");
+        }
+        else if (percent >= 70)
+        {
+            Console.WriteLine($"{gradeC}{gradePass}");
+        }
+        else if (percent >= 60)
+        {
+            Console.WriteLine($"{gradeD}{gradeFail}");
+        }
+        else
+        {
+            Console.WriteLine($"{gradeF}{gradeFail}");
+        }
     }
 }
