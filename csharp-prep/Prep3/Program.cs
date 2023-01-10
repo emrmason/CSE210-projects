ab4c719aa46f552a7034bdf7cc1001aea99487cd
emrmason <mas20021@byui.edu>
2023-01-09 17:34:12 -0800

    Prep 3 Attempt 1- Still buggy.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Welcome to the number guessing game!");
+        Console.WriteLine("Would you like to play? ");
+        string play = Console.ReadLine();
+
+        do {
+        Console.WriteLine("Please enter the magic number. ");
+        int magicNumber = Int16.Parse(Console.ReadLine());
+        Console.WriteLine("Guess the magic number: ");
+        int guess = Int16.Parse(Console.ReadLine());
+        if (guess > magicNumber)
+        {
+            Console.WriteLine("Try a lower number. ");
+            Console.ReadLine();
+        }
+        else if (guess < magicNumber)
+        {
+            Console.WriteLine("Try a higher number. ");
+            Console.ReadLine();
+
+        }
+        else
+        {
+            Console.WriteLine("You guessed it! ");
+
+        }
+        } while (play == "yes");
     }
 }
