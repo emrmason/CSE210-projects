2f6562a7d2955a383bd4d4a40a24d93fbe0ecc38
emrmason <mas20021@byui.edu>
2023-01-10 19:13:36 -0800

    Prep 3 Updated with guess counter added

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -9,6 +9,8 @@ class Program
         Console.WriteLine("Please enter the magic number. ");
         int magicNumber = Int16.Parse(Console.ReadLine());
 
+        int i = 0;
+
 // Had help from a github user: https://gist.github.com/MrAwesomeness/6196354b6643faaad801  No code copied, just concepts used.
 // Learned that my guess write and readlines needed to be INSIDE the while loop in order for the user to be able to guess more than once.
 
@@ -16,22 +18,23 @@ class Program
         {
             Console.WriteLine("Guess the magic number: ");
             guess = Int16.Parse(Console.ReadLine());  
+            i++; 
 
             if (guess > magicNumber)
             {
-            Console.WriteLine("Try a lower number.");
+            Console.WriteLine($"Try a lower number. This is guess {i}.");
             }
 
             else if (guess < magicNumber)
             {
-            Console.WriteLine("Try a higher number.");
+            Console.WriteLine($"Try a higher number. This is guess {i}.");
             }  
 
         }
 
         if (guess == magicNumber)
         {
-            Console.WriteLine("You guessed it! Way to go!");
+            Console.WriteLine($"You guessed it after {i} tries! Way to go!");
         }
 }
 }
