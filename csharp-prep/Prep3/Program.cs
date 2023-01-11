4ae0aa060a0615a0967222e933f64faab5c16e68
emrmason <mas20021@byui.edu>
2023-01-10 19:07:05 -0800

    Prep 3 Assignment- Core Requirements Met

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,31 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
+        int guess = 0;
         Console.WriteLine("Welcome to the number guessing game!");
-        Console.WriteLine("Would you like to play? ");
-        string play = Console.ReadLine();
-
-        do {
         Console.WriteLine("Please enter the magic number. ");
         int magicNumber = Int16.Parse(Console.ReadLine());
-        Console.WriteLine("Guess the magic number: ");
-        int guess = Int16.Parse(Console.ReadLine());
-        if (guess > magicNumber)
+
+// Had help from a github user: https://gist.github.com/MrAwesomeness/6196354b6643faaad801  No code copied, just concepts used.
+
+        while (guess != magicNumber)
         {
-            Console.WriteLine("Try a lower number. ");
-            Console.ReadLine();
-        }
-        else if (guess < magicNumber)
-        {
-            Console.WriteLine("Try a higher number. ");
-            Console.ReadLine();
+            Console.WriteLine("Guess the magic number: ");
+            guess = Int16.Parse(Console.ReadLine());  
+
+            if (guess > magicNumber)
+            {
+            Console.WriteLine("Try a lower number.");
+            }
+
+            else if (guess < magicNumber)
+            {
+            Console.WriteLine("Try a higher number.");
+            }  
 
         }
-        else
-        {
-            Console.WriteLine("You guessed it! ");
 
+        if (guess == magicNumber)
+        {
+            Console.WriteLine("You guessed it! Way to go!");
         }
-        } while (play == "yes");
-    }
+}
 }
