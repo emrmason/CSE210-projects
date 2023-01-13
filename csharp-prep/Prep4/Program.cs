8ada3806b7980be94ab91056d3ece747ef0b7649
emrmason <mas20021@byui.edu>
2023-01-13 13:59:18 -0800

    Completed C# Prep 4 Core Requirements.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int newEntry = Int16.Parse(Console.ReadLine());
+   
+        while (newEntry != 0)
+        {        
+            numbers.Add(newEntry);
+            Console.WriteLine("Enter another number:");
+            newEntry = Int16.Parse(Console.ReadLine());
+        }
+        
+        if (newEntry == 0)
+        {
+            double summation = numbers.Sum();
+            double avg1 = numbers.Average();
+            double max1 = numbers.Max();
+            double min1 = numbers.Min();
+            Console.WriteLine($"The sum of your list is: {summation}. The average of your list is: {avg1}."); 
+            Console.WriteLine($"The highest value you provided is: {max1}. The lowest value you provided is: {min1}.");
+        }
     }
 }
