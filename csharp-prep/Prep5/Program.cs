ca384d451233c32b6d88e6dab5e3137be8195f0c
emrmason <mas20021@byui.edu>
2023-01-13 20:59:34 -0800

    Completed Prep 5 program

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        {
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+        int favNumber = PromptUserNumber();
+        int sqNumber = SquareNumber(favNumber);
+
+        DisplayResult(userName, sqNumber);
+
+        }
+
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the C# Prep 5 Program!");
+        }
+
+        static string PromptUserName() 
+        {
+            Console.Write("What is your name? ");
+            string userName = Console.ReadLine();
+            return userName;
+        }
+            
+        static int PromptUserNumber()
+        {
+            Console.Write("What is your favorite number? ");
+            int favNumber = Int16.Parse(Console.ReadLine());
+            return favNumber;
+        }
+
+        static int SquareNumber(int number)
+        {
+            int square = number * number;
+            return square;
+        }
+
+        static void DisplayResult(string userName, int square)
+        {
+            Console.WriteLine($"{userName}, the square of your favorite number is {square}.");
+        }
     }
 }
