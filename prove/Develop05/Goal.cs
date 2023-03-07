0ca786b21fd8f35043527938662f56c6ae961946
emrmason <mas20021@byui.edu>
2023-03-06 21:45:21 -0800

    Menu working, goal class created.

---
+++ prove/Develop05/Goal.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Goal
+{
+    protected string _name;
+    protected string _description;
+    protected int _points;
+
+    public List<Goals> goals = new List<Goals>;
+
+    public Goal()
+    {
+
+    }
+}
--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,6 +4,53 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        bool startGoals = true;
+        Console.WriteLine("\nWelcome to the Goals program. Please select from the following menu options: \n");
+
+        while(startGoals == true)
+        {
+            Menu menu = new Menu();
+            menu.displayMenu();
+            Console.Write("\nEnter your selection: ");
+            int choice = Int32.Parse(Console.ReadLine());
+
+            if(choice == 1)
+            {
+                Console.WriteLine("Create a Goal");
+                startGoals = true;
+            }
+            if(choice == 2)
+            {
+                Console.WriteLine("List Goals");
+                startGoals = true;
+            }
+            if(choice == 3)
+            {
+                Console.WriteLine("Save Goals");
+                startGoals = true;
+            }
+            if(choice == 4)
+            {
+                Console.WriteLine("Load Goals");
+                startGoals = true;
+            }
+            if(choice == 5)
+            {
+                Console.WriteLine("Record Events");
+                startGoals = true;
+            }
+            if(choice == 6)
+            {
+                Console.WriteLine("Well done for working on your goals!");
+                startGoals = false;
+            }
+            else
+            {
+                Console.WriteLine("\nPlease make a valid selection from the list. \n");
+                startGoals = true;
+            }
+
+        }
+
     }
 }
