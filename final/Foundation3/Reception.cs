1f8ae13abaf7382b126184da2b45d843683178d8
emrmason <mas20021@byui.edu>
2023-03-31 13:28:25 -0700

    Finishing touches on Events Program

--- final/Foundation3/Reception.cs
+++ final/Foundation3/Reception.cs
@@ -18,16 +18,16 @@ class Reception : Event
     }
         public override string getStandard()
     {
-        return $"     *****     \n{_title}\n{_description}\n {_date}, at {_time}\n{_address}\n     *****     ";
+        return $"\n          *****          \n{_title}\n{_description}\n {_date}, at {_time}\n{_address}\n          *****          \n";
     }
 
     public override string getDetails()
     {
-        return $"  **  **  **  **  **  \nWe are pleased to announce the Wedding of{_title}\n{_description}\n Please Join Us to Celebrate:\n{_date}, from {_time}\n We will be celebrating at {_address}\n\n >>>  Please RSVP at {_emailRSVP} <<< \n  **  **  **  **  **  \n ";
+        return $"\n  **  **  **  **  **  **  **  **  \nWe are pleased to announce the {_title}\n{_description}\n Please Join Us to Celebrate:\n{_date}, from {_time}\n We will be celebrating at {_address}\n\n >>>  Please RSVP at {_emailRSVP} <<< \n  **  **  **  **  **  **  **  **  \n ";
     }
 
     public override string getShort()
     {
-        return "*** Save the Date *** ";
+        return $"\n*** Save the Date *** \n{_title}\n{_date}\n   ***      ***      ***   \n";
     }
 }
