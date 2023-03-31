29595e88f0220d2108eb004532bd04c0ee7e5c38
emrmason <mas20021@byui.edu>
2023-03-30 19:23:13 -0700

    Foundation project 3 almost complete

--- final/Foundation3/Event.cs
+++ final/Foundation3/Event.cs
@@ -5,7 +5,7 @@ class Event
 {
     protected string _title;
     protected string _description;
-    protected string _type;
+    public string _type; 
     protected string _date;
     protected string _time;
     protected string _address;
@@ -14,16 +14,31 @@ class Event
     {
 
     }
-    public Event(string title, string desc, string type, string date, string time, string addy)
+    public virtual string getEvent(string title, string desc, string type, string date, string time, string address)
     {
         _title = title;
         _description = desc;
         _type = type;
         _date = date;
         _time = time;
-        _address = addy;
-        _eventSummary = $"{_title}, {_description}, {_date} {_time} at {_address}.";
+        _address = address;
+        _eventSummary = $"{_title}, {_description} ({_type})\n{_date}, at {_time}\n{_address}";
+        return _eventSummary;
 
+    }
+
+    public virtual string getStandard()
+    {
+        return "";
+    }
+    public virtual string getDetails()
+    {
+        return "";
+    }
+
+    public virtual string getShort()
+    {
+        return "";
     }
 
 }
--- final/Foundation3/Reception.cs
+++ final/Foundation3/Reception.cs
@@ -4,16 +4,30 @@ using System.Collections;
 class Reception : Event
 {
     protected string _emailRSVP;
-    protected int _haveRSVPd;
+
+    public string _eventType = "Reception";
 
     public Reception()
     {
 
     }
 
-    public Reception(string email, int rsvp)
+    public Reception(string email)
     {
         _emailRSVP = email;
-        _haveRSVPd = rsvp;
+    }
+        public override string getStandard()
+    {
+        return $"     *****     \n{_title}\n{_description}\n {_date}, at {_time}\n{_address}\n     *****     ";
+    }
+
+    public override string getDetails()
+    {
+        return $"  **  **  **  **  **  \nWe are pleased to announce the Wedding of{_title}\n{_description}\n Please Join Us to Celebrate:\n{_date}, from {_time}\n We will be celebrating at {_address}\n\n >>>  Please RSVP at {_emailRSVP} <<< \n  **  **  **  **  **  \n ";
+    }
+
+    public override string getShort()
+    {
+        return "*** Save the Date *** ";
     }
 }
